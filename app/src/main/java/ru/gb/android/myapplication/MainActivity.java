e201fd61703fad5543011d42b7847200133fdb4e
mainnsk <gamemainnsk@gmail.com>
2021-09-29 07:01:17 +0700

    Lesson2: add clicklisteners

--- app/src/main/java/ru/gb/android/myapplication/MainActivity.java
+++ app/src/main/java/ru/gb/android/myapplication/MainActivity.java
@@ -1,14 +1,40 @@
 package ru.gb.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private View firstView;
+    private AppCompatImageView logIv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setupListeners();
+
+    }
+    private void setupListeners(){
+
+        firstView.setOnClickListener(v ->{
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        });
+        logIv.setOnClickListener(v-> {
+            Snackbar.make(v,"Hello", Snackbar.LENGTH_SHORT).show();
+        });
+    }
+    private void initViews() {
+        firstView = findViewById(R.id.first_view);
+        logIv = findViewById(R.id.button_logo);
     }
 }
